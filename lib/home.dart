cda82aec81af9445a7c1d46448f9435ba0c5753e
Raza <62023571+aminrazakhan07@users.noreply.github.com>
2024-11-16 19:11:26 +0500

    payment get

---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Text('Payment Getway'),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
